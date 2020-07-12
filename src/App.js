af60b4327fb3eea30d37430f456628ea4223a3d0
bajenovjsdeveloper <bajenovcpm433@yandex.ru>
2020-07-13 00:34:48 +0300

    Add hourly discription on each day

--- src/App.js
+++ src/App.js
@@ -1,50 +1,134 @@
 import React, { useState, useEffect } from 'react';
 import './App.css';
 import * as axios from 'axios';
-import { Route, Switch, Redirect} from 'react-router-dom';
-import Weather from './Weather';
+import {
+  Route, Switch, Redirect, useHistory,
+} from 'react-router-dom';
+import Weather, { Grafic, Hourly } from './Weather';
+
 import loadingSpin from './Rolling-1s-200px.png';
 
-const WeatherHourly = () =>{
+const WeatherDayDisc = ({ wdDiscr, date }) => (
+  <React.Fragment>
+    <p className="hh-day">
+      {wdDiscr.weekDay}
+      <span>{date}</span>
+    </p>
+    <p className="hh-disc">{wdDiscr.discription}</p>
+    <div className="hh-main">
+      <div>
+        <img src={`https://openweathermap.org/img/wn/${wdDiscr.curTimeWeatherImg}@2x.png`} />
+      </div>
+      <div className="hh-temp">
+        <p>{`${wdDiscr.temperature}`}&deg;</p>
+      </div>
+      <div className="hh-info">
+        <p>{`preasure: ${wdDiscr.pressure} kPa`}</p>
+        <p>{`humidity: ${wdDiscr.humidity}%`}</p>
+        <p>{`wind speed: ${wdDiscr.windSpeed}m/s`}</p>
+        <p>{`wind direction: ${wdDiscr.windDegrees}`}&deg;</p>
+        <p></p>
+      </div>
+    </div>
+  </React.Fragment>
+);
+
+const WeatherGrafic = ({ currentDay, tempsArr }) => {
+  const grafic = new Grafic(600, 200);
+
+  useEffect(() => {
+    grafic.drawTemps(tempsArr);
+  }, [currentDay]);
 
   return (
-      <div className='container-hourly'>
+    <React.Fragment>
+      <p className="canv-temp">Temperature &deg;C</p>
+      <canvas width="600px" height="200px" id="grafic"></canvas>
+    </React.Fragment>
+  );
+};
 
+const WeatherHourly = ({ listTable, currentItemId }) => {
+  const hourly = new Hourly(listTable, currentItemId);
+  const tempsArr = hourly.getTArray();
+  const isVisible = !!listTable;
+  const curDate = isVisible ? listTable[currentItemId].date : '';
+  const wdDiscr = {
+    weekDay: hourly.getWeekDay(),
+    pressure: hourly.getPpressure(),
+    humidity: hourly.getHumidity(),
+    temperature: hourly.getMaxTemp(),
+    windSpeed: hourly.getWindSpeed(),
+    windDegrees: hourly.getWindDir(),
+    discription: hourly.getDiscription(),
+    curTimeWeatherImg: hourly.getCurImg(),
+  };
+
+  return (
+    <div className="container-hourly">
+      <div className="h-header">
+        <WeatherDayDisc wdDiscr={wdDiscr} date={curDate} />
       </div>
-    );
-}
-
-
-
-const WeatherMap = ({ dataList, visible, handleClick }) =>
-    (
-      visible && (
-      <div className="card-list">
-        {dataList.map((item, idx) => (
-          <div className="card" key={idx} id={idx} onClick={() => handleClick(idx)}>
-            <p className="card-day">{item.day}</p>
-            <img
-              src={`https://openweathermap.org/img/wn/${item.img}@2x.png`}
-              className="card-img"
-              alt="weather-igm"
-            />
-            <p className="card-temp-max">
-              {item.temp[1]}&deg; - <span className="card-temp-min">{item.temp[0]}&deg;</span>
-            </p>
-          </div>
-        ))}
+      <div className="h-side">
+        <p>Temperature&deg;C</p>
       </div>
-    )
+      <div className="h-main">
+        <WeatherGrafic tempsArr={tempsArr} currentDay={currentItemId} />
+      </div>
+      <div className="h-footer">
+        {isVisible
+          && listTable[currentItemId].hourly.map((w, idx) => {
+            const date = new Date(w.dt_txt);
+            const dayNow = new Date().getDate();
+            const time = `${date.getHours().toString().padStart(2, '0')}:
+                        ${date.getMinutes().toString().padStart(2, '0')}`;
+            let active = '';
+            const img = w.weather[0].icon;
+            const rain = w.rain ? w.rain['3h'] : null;
+            if (date.getDate() === dayNow && idx === 0) active = 'active';
+            return (
+              <div className={`hf-section ${active}`} key={time}>
+                <p className="hf-time">{time}</p>
+                <img className="hf-img" src={`https://openweathermap.org/img/wn/${img}@2x.png`} />
+                {rain && <p className="hf-rain">{rain} mm</p>}
+              </div>
+            );
+          })}
+      </div>
+    </div>
+  );
+};
+
+const WeatherMap = ({ dataList, visible, handleClick }) => visible && (
+    <div className="card-list">
+      {dataList.map((item, idx) => (
+        <div className="card" key={idx} id={idx} onClick={() => handleClick(idx)}>
+          <p className="card-day">{item.day.slice(0, 3)}</p>
+          <img
+            src={`https://openweathermap.org/img/wn/${item.img}@2x.png`}
+            className="card-img"
+            alt="weather-igm"
+          />
+          <p className="card-temp-max">
+            {item.temp[1]}&deg; - <span className="card-temp-min">{item.temp[0]}&deg;</span>
+          </p>
+        </div>
+      ))}
+    </div>
 );
 
 function App({ init }) {
   const weather = new Weather();
   const [loading, setLoading] = useState(true);
-  const [dataList, setDataList] = useState({});
+  const [dataList, setDataList] = useState(null);
   const [city, setCity] = useState('');
+  const [curItem, setCurItem] = useState(0);
+  const hisory = useHistory();
 
   const handleClick = (item) => {
     console.log(item);
+    setCurItem(item);
+    hisory.push(`/weather-page/dayly/${item}`);
   };
 
   useEffect(() => {
@@ -55,17 +139,17 @@ function App({ init }) {
       .then((response) => {
         console.log(response);
         if (response.statusText === 'OK') {
-          if(!weather.init(response.data)) throw 'Data not initialized!';
+          if (!weather.init(response.data)) throw 'Data not initialized!';
           console.log(weather);
           const weatherArray = weather.getDateArray();
           setDataList(
             weatherArray.map((item) => {
               const obj = {
                 img: weather.getIcon(),
-                // city: weather.getCity(),
                 temp: weather.getMinMaxTemp('C'),
-                // date: weather.getCurDateTime('DMY'),
+                date: weather.getCurDateTime('DMY'),
                 day: weather.getDayOfWeek(),
+                hourly: weather.getWeatherHourly(item.dt_txt),
               };
               weather.nextDate();
               return obj;
@@ -82,34 +166,41 @@ function App({ init }) {
       });
   }, [init]);
   return (
-      <div className="App">
-        <header className="App-header">
-          <h1>
-            Weather
-          </h1>
-        </header>
-        <article>
-          <div className="container">
-            <p className="container-city">{city}</p>
-            {loading && (
-              <div>
-                <img src={loadingSpin} alt="loading" className="loading" />
-                <p>In progress...</p>
-              </div>
-            )}
-            <Switch>
-              <Route
-                exact path="/weather-page"
-                component={() => (
-                  <WeatherMap key='weather' dataList={dataList} handleClick={handleClick} visible={!loading} />
-                )}
-              />
-              <Route path='/weather-page/hourly' component={() => ( <WeatherHourly /> )}/>
-              <Redirect to='/weather-page' />
-            </Switch>
-          </div>
-        </article>
-      </div>
+    <div className="App">
+      <header className="App-header">
+        <h1>Weather</h1>
+      </header>
+      <article>
+        <div className="container">
+          <p className="container-city">{city}</p>
+          {loading && (
+            <div>
+              <img src={loadingSpin} alt="loading" className="loading" />
+              <p>In progress...</p>
+            </div>
+          )}
+          <Switch>
+            <Route
+              exact
+              path="/weather-page"
+              component={() => (
+                <WeatherMap
+                  key="weather"
+                  dataList={dataList}
+                  handleClick={handleClick}
+                  visible={!loading}
+                />
+              )}
+            />
+            <Route
+              path="/weather-page/dayly"
+              component={() => !loading && <WeatherHourly listTable={dataList} currentItemId={curItem} />
+              }
+            />
+          </Switch>
+        </div>
+      </article>
+    </div>
   );
 }
 
--- src/Weather.js
+++ src/Weather.js
@@ -8,30 +8,55 @@ class Weather {
     // F = 1.8 * (K-273) + 32;
     // C = K - 273
     this._convertToC = (temp = 0) => Math.round(temp - 273);
-    this._convertToF = (temp = 0) => Math.round(1.8 * (temp - 273) + 32)
+    this._convertToF = (temp = 0) => Math.round(1.8 * (temp - 273) + 32);
   }
 
   getSize() {
-   if(this._initialize) return this.listDateArray.length;
+    if (this._initialize) return this.listDateArray.length;
+    return null;
   }
 
   getIcon(opt = 0) {
-    if(this._initialize){
+    if (this._initialize) {
       return this.listDateArray[this._currentDay].weather[0].icon;
     }
+    return null;
   }
 
   getCity() {
-    if(this._initialize){
+    if (this._initialize) {
       return this._data.city.name;
     }
+    return null;
+  }
+
+  getWeatherHourly(date) {
+    const hourArr = [];
+    if (this._initialize) {
+      const dateNow = new Date();
+      const initialDay = new Date(date).getDate();
+      this._data.list.forEach((item) => {
+        const curDay = new Date(item.dt_txt).getDate();
+        if (curDay === initialDay) {
+          hourArr.push(item);
+        }
+      });
+
+      if (initialDay === dateNow.getDate() && hourArr.length < 8) {
+        const len = hourArr.length;
+        for (let i = 0; i < 8 - len; i++) {
+          // -- add some elements, which`s need to be 8
+          hourArr.push(this._data.list[len + i]);
+        }
+      }
+    }
+    return hourArr;
   }
 
   getMinMaxTemp(option = 'C') {
-   
     if (this._initialize) {
       const temps = [];
-      const convTempFunc = (option === 'F')? this._convertToF : this._convertToC; 
+      const convTempFunc = option === 'F' ? this._convertToF : this._convertToC;
       this._data.list.forEach((item) => {
         const d = new Date(item.dt_txt);
         if (d.getDate() === this._initialDay + this._currentDay) {
@@ -48,7 +73,7 @@ class Weather {
     if (this._initialize) {
       return this.listDateArray[this._currentDay].weather[0].description;
     }
-    // return null;
+    return null;
   }
 
   getDateArray() {
@@ -74,15 +99,14 @@ class Weather {
     return false; // -- not initialized
   }
 
-  getCurrentTemp() {
-    if (this._initialize) {
-      return (
-        this._convertToC(this.listDateArray[this._currentDay].main.temp)
-            .toString() || 'Unkown.'
-      );
-    }
-    // return null;
-  }
+  // getCurrentTemp() {
+  //   if (this._initialize) {
+  //     return (
+  //       this._convertToC(this.listDateArray[this._currentDay].main.temp).toString() || 'Unkown.'
+  //     );
+  //   }
+  //   // return null;
+  // }
 
   nextDate() {
     if (this._currentDay < 6) {
@@ -111,11 +135,145 @@ class Weather {
 
   getDayOfWeek() {
     if (this._initialize) {
-      const days = ['Sun', 'Mon', 'Tue', 'Wed', 'Thu', 'Fri', 'Sat'];
+      const days = ['Sunday', 'Monday', 'Tuesday', 'Wednesday', 'Thusday', 'Friday', 'Saturday'];
       return days[new Date(this.listDateArray[this._currentDay].dt_txt).getDay()];
     }
     return null;
   }
 }
 
+class Grafic {
+  constructor(width, height) {
+    this.width = width;
+    this.height = height;
+  }
+
+  drawTemps(arr) {
+    const k = Math.round(this.width / 8);
+    const canvas = document.getElementById('grafic');
+
+    if (canvas && arr.length > 0) {
+      const { height } = this;
+      const ctx = canvas.getContext('2d');
+      let offsetX = k - 37.5;
+      const dY = 2;
+      const grad = ctx.createLinearGradient(0, 200, 0, 0);
+      grad.addColorStop(0, 'white');
+      grad.addColorStop(1, 'orange');
+
+      ctx.strokeStyle = 'black';
+      ctx.lineWidth = 1;
+      ctx.fillStyle = grad;
+      ctx.beginPath();
+      ctx.moveTo(0, height);
+      ctx.lineTo(0, height - (k + arr[0] * dY));
+
+      arr.forEach((i) => {
+        ctx.lineTo(offsetX, height - (k + i * dY));
+        offsetX += k;
+      });
+      ctx.lineTo(offsetX, height - (k + arr[arr.length - 1] * dY));
+      ctx.lineTo(offsetX, height);
+      ctx.stroke();
+      ctx.fill();
+      ctx.closePath();
+
+      ctx.beginPath();
+      offsetX = k - 37.5;
+      ctx.fillStyle = 'white';
+      arr.forEach((i) => {
+        ctx.beginPath();
+        ctx.arc(offsetX, height - (k + i * dY), 2, 0, Math.PI * 2);
+        ctx.strokeText(i.toString(), offsetX - 5, height - (k + i * dY) - 5);
+        offsetX += k;
+        ctx.stroke();
+        ctx.fill();
+        ctx.closePath();
+      });
+    }
+  }
+}
+
+class Hourly {
+  constructor(listTable, id) {
+    this._list = listTable;
+    this.id = id;
+    this._hoCuItem = null;
+    this._init = false;
+    if (listTable) {
+      this._hoCuItem = listTable[id].hourly[0];
+      this._init = listTable[id].hourly.length >= 0;
+    }
+  }
+
+  getPpressure() {
+    if (this._init) {
+      const pressure = this._list[this.id].hourly.map((i) => i.main.pressure);
+      return Math.max(...pressure);
+    }
+    return null;
+  }
+
+  getWeekDay() {
+    if (this._init) {
+      return this._list[this.id].day;
+    }
+    return null;
+  }
+
+  getTArray() {
+    if (this._init) {
+      return this._list[this.id].hourly.map((i) => Math.round(i.main.temp - 273));
+    }
+    return [];
+  }
+
+  getCurImg() {
+    if (this._init) {
+      return this._hoCuItem.weather[0].icon;
+    }
+    return null;
+  }
+
+  getHumidity() {
+    if (this._init) {
+      const hum = this._list[this.id].hourly.map((i) => i.main.humidity);
+      return Math.max(...hum);
+    }
+    return null;
+  }
+
+  getMaxTemp() {
+    if (this._init) {
+      let temp = this._list[this.id].hourly.map((i) => i.main.temp);
+      temp = Math.max(...temp);
+      return Math.round(temp - 273);
+    }
+    return null;
+  }
+
+  getWindSpeed() {
+    if (this._init) {
+      const wind = this._list[this.id].hourly.map((i) => i.wind.speed);
+      return Math.max(...wind);
+    }
+    return null;
+  }
+
+  getWindDir() {
+    if (this._init) {
+      return this._hoCuItem.wind.deg;
+    }
+    return null;
+  }
+
+  getDiscription(date) {
+    if (this._init) {
+      return this._list[this.id].hourly[0].weather[0].description;
+    }
+    return null;
+  }
+}
+
+export { Grafic, Hourly };
 export default Weather;
